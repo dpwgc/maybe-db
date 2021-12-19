3b85b6376dae74674581544808ad6a12d274d8e7
帅逼 <281148314@qq.com>
2021-12-19 17:43:30 +0800

    日志记录完善

--- middlewares/safeMiddleware.go
+++ middlewares/safeMiddleware.go
@@ -1,7 +1,6 @@
 package middlewares
 
 import (
-	"MaybeDB/servers"
 	"github.com/gin-gonic/gin"
 	"github.com/spf13/viper"
 )
@@ -12,11 +11,6 @@ func SafeMiddleWare(c *gin.Context) {
 	secretKey := c.GetHeader("secretKey")
 
 	if secretKey != viper.GetString("db.secretKey") {
-
-		sourceIp := c.ClientIP()
-		accessUri := c.Request.URL.Path
-
-		servers.Loger.Println("Illegal access , sourceIp: " + sourceIp + " , accessUri: " + accessUri)
 
 		c.JSON(-1, gin.H{
 			"code": -1,
